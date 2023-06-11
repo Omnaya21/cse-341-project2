242de9dc480420181d7bca737eca7926f953b466
Omar Navarro <omar.navarroy@yahoo.com>
2023-06-11 01:58:42 -0700

    Added code for personal assignment 2

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+const professionalRoutes = require('./routes/professional');
 
 const port = process.env.PORT || 8080;
 const app = express();
@@ -10,8 +11,9 @@ app
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-  });
-  //.use('/', require('/routes'));
+  })
+  //.use('/professional', professionalRoutes);
+  .use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
