bcc44c71cf9ce49b83ecc4a676e921e461a8cf5c
Omar Navarro <omar.navarroy@yahoo.com>
2023-06-11 00:36:47 -0700

    Add mongo db connection

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('./db/connect');
+
+const port = process.env.PORT || 8080;
 const app = express();
 
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  });
+  //.use('/', require('/routes'));
+
+mongodb.initDb((err, mongodb) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to Mongo DB and listening on port ${port}`);
+  }
+});
+
+/*
 app.get('/', (req, res) => {
   res.send('Hello REST 2.0');
 });
 
 app.listen(process.env.PORT || 8080);
 console.log('Web Server is listening at port ' + (process.env.PORT || 8080));
+*/
